aaf1f502bd0d54dba3cf9c8c03b7522e56c8feb9
SHIMODA Hiroshi <shimoda@clear-code.com>
2010-11-09 16:43:00 +0900

    buildscript submodule

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 setlocal
 set appname=restartless
 
-copy buildscript\make_new.sh .\
-bash make_new.sh %appname% version=0
-del make_new.sh
+copy buildscript\makexpi.sh .\
+bash makexpi.sh %appname% version=0
+del makexpi.sh
 endlocal
