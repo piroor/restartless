f0829b28415ba0901ce970546507948536563fda
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2011-02-28 00:04:22 +0900

    update jsdeferred

--- modules/lib/jsdeferred.js
+++ modules/lib/jsdeferred.js
@@ -1,34 +1,45 @@
 // Usage:: var Deferred = require('..../jsdeferred.bootstrap.js');
 // JSDeferred 0.3.4 Copyright (c) 2007 cho45 ( www.lowreal.net )
 // See http://github.com/cho45/jsdeferred
-var EXPORTED_SYMBOLS = ['Deferred'];
-var window = {};
-var location = { protocol: 'resource:' };
-var document = { addEventListener : function() {} };
+var EXPORTED_SYMBOLS = ["Deferred"];
 var timers = [];
 
-function setTimeout (aCallback, aDelay) {
-	var timer = Components.classes['@mozilla.org/timer;1']
-					.createInstance(Components.interfaces.nsITimer);
-	timer.initWithCallback(function() {
-		timers.splice(timers.indexOf(timer), 1);
-		aCallback();
-	}, aDelay, timer.TYPE_ONE_SHOT);
-	timers.push(timer);
-	return timer;
+function setTimeout (f, i) {
+	if (i) {
+		let timer = Components.classes["@mozilla.org/timer;1"]
+						.createInstance(Components.interfaces.nsITimer);
+		timer.initWithCallback(f, i, timer.TYPE_ONE_SHOT);
+		timers.push(timer);
+		return timer;
+	}
+	else {
+		let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
+					.createInstance(Components.interfaces.nsIXMLHttpRequest)
+					.QueryInterface(Components.interfaces.nsIDOMEventTarget);
+		let handler = function(e) {
+				if (request.readyState < 2) return;
+				request.removeEventListener("readystatechange", handler, false);
+				f();
+			};
+		request.open("GET", "data:,", true);
+		request.addEventListener("readystatechange", handler, false);
+		request.send(null);
+		timers.push(request);
+		return request;
+	}
 }
 
-function clearTimeout(aTimer) {
-	timers.splice(aTimer.indexOf(timer), 1);
-	aTimer.cancel();
+function clearTimeout (timer) {
+	timers.splice(timers.indexOf(timer), 1);
+	if (timer instanceof Components.interfaces.nsITimer)
+		timer.cancel();
+	else
+		timer.abort();
 }
 
 function shutdown()
 {
-	timers.forEach(function(aTimer) {
-		clearTimeout(aTimer);
-	});
-	delete exports.Deferred;
+	timers.forEach(clearTimeout);
 	setTimeout = void(0);
 	clearTimeout = void(0);
 	timers = void(0);
