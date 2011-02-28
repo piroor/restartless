4197cdb0c6395855686178ab38a5f2be9d49ef9f
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2011-02-28 23:09:04 +0900

    update jsdeferred

--- modules/lib/jsdeferred.js
+++ modules/lib/jsdeferred.js
@@ -5,36 +5,16 @@ var EXPORTED_SYMBOLS = ["Deferred"];
 var timers = [];
 
 function setTimeout (f, i) {
-	if (i) {
-		let timer = Components.classes["@mozilla.org/timer;1"]
-						.createInstance(Components.interfaces.nsITimer);
-		timer.initWithCallback(f, i, timer.TYPE_ONE_SHOT);
-		timers.push(timer);
-		return timer;
-	}
-	else {
-		let request = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
-					.createInstance(Components.interfaces.nsIXMLHttpRequest)
-					.QueryInterface(Components.interfaces.nsIDOMEventTarget);
-		let handler = function(e) {
-				if (request.readyState < 2) return;
-				request.removeEventListener("readystatechange", handler, false);
-				f();
-			};
-		request.open("GET", "data:,", true);
-		request.addEventListener("readystatechange", handler, false);
-		request.send(null);
-		timers.push(request);
-		return request;
-	}
+	let timer = Components.classes["@mozilla.org/timer;1"]
+					.createInstance(Components.interfaces.nsITimer);
+	timer.initWithCallback(f, i, timer.TYPE_ONE_SHOT);
+	timers.push(timer);
+	return timer;
 }
 
 function clearTimeout (timer) {
 	timers.splice(timers.indexOf(timer), 1);
-	if (timer instanceof Components.interfaces.nsITimer)
-		timer.cancel();
-	else
-		timer.abort();
+	timer.cancel();
 }
 
 function shutdown()
