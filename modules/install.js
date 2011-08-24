78c4edeeff656c1b260393ac70a9abdee406e7b5
Piro / SHIMODA Hiroshi <piro@p.club.ne.jp>
2011-08-25 02:38:06 +0900

    Install/uninstall callbacks should be defined in the special module "install.js" instead of "main.js". Disabled addon's main module should not be loaded. (fix for https://github.com/piroor/restartless/issues/1 )

---
+++ modules/install.js
@@ -0,0 +1,22 @@
+/**
+ * @fileOverview Installation module for restartless addons
+ * @author       SHIMODA "Piro" Hiroshi
+ * @version      1
+ *
+ * @license
+ *   The MIT License, Copyright (c) 2011 SHIMODA "Piro" Hiroshi.
+ *   https://github.com/piroor/restartless/blob/master/license.txt
+ * @url http://github.com/piroor/restartless
+ */
+
+dump('install.js loaded\n');
+
+function install()
+{
+	dump('install!\n');
+}
+
+function uninstall()
+{
+	dump('uninstall!\n');
+}
