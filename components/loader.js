84dc5e25ede02a75ee9e9c355a70440fab6cb602
YUKI Hiroshi <shimoda@clear-code.com>
2015-08-31 15:21:41 +0900

    Remove support of doAndWait.
    because processNextEvent is deprecated.

--- components/loader.js
+++ components/loader.js
@@ -2,10 +2,10 @@
  * @fileOverview Loader module for restartless addons
  * @author       YUKI "Piro" Hiroshi
  * @contributor  Infocatcher
- * @version      12
+ * @version      13
  *
  * @license
- *   The MIT License, Copyright (c) 2010-2014 YUKI "Piro" Hiroshi.
+ *   The MIT License, Copyright (c) 2010-2015 YUKI "Piro" Hiroshi.
  *   https://github.com/piroor/restartless/blob/master/license.txt
  * @url http://github.com/piroor/restartless
  */
@@ -208,33 +208,6 @@ function exists(aPath, aBaseURI)
 	}
 }
 
-function doAndWait(aAsyncTask)
-{
-	const Cc = Components.classes;
-	const Ci = Components.interfaces;
-
-	var done = false;
-	var returnedValue = void(0);
-	var continuation = function(aReturnedValue) {
-			done = true;
-			returnedValue = aReturnedValue;
-		};
-
-	var timer = Cc['@mozilla.org/timer;1']
-					.createInstance(Ci.nsITimer);
-	timer.init(function() {
-		aAsyncTask(continuation);
-	}, 0, Ci.nsITimer.TYPE_ONE_SHOT);
-
-	var thread = Cc['@mozilla.org/thread-manager;1']
-					.getService(Ci.nsIThreadManager)
-					.currentThread;
-	while (!done) {
-		thread.processNextEvent(true);
-	}
-	return returnedValue;
-}
-
 function _readFrom(aURISpec, aEncoding)
 {
 	const Cc = Components.classes;
@@ -338,9 +311,6 @@ function _createNamespace(aURISpec, aRoot)
 			/* utility to read contents of a text file */
 			read : function(aURISpec, aEncoding, aBaseURI) {
 				return _readFrom(this.resolve(aURISpec, aBaseURI), aEncoding);
-			},
-			doAndWait : function(aAsyncTask) {
-				return doAndWait(aAsyncTask);
 			},
 			exports : {}
 		});
